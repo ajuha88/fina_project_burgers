610edc6cd619628fb6115c1cf39752bf73515459
Bootcamp Work <ramos_conde@yahoo.com>
2019-05-09 17:26:32 -0700

    nothing changed

--- static/JS/logic.js
+++ static/JS/logic.js
@@ -34,7 +34,7 @@ for (var i = 0; i < data.data.length; i++) {
 	var location = data.data[i];
   if (location.Category === "Burger Joint"){
   	burgersMarkers.addLayer(
-    L.marker([location.lats, location.lngs]).bindPopup("<h5>Category: " + location.Category + "</h5><hr><h5>Neighborhood: " + location.Neighborhood + "</h5><hr><h5>Venue: " + location.Venue + "</h5><h5>Rating: " + location.Rating + "</h5>"))
+    L.marker([location.lats, location.lngs]).bindPopup("<h5>Category: " + location.Category + "</h5><hr><h5>Neighborhood: " + location.Neighborhood + "</h5><hr><h5>Venue: " + location.Venue + "</h5><h5>Rating: " + location.Rating + " and " + location.Count + " reviews</h5>"))
   } 
     //   else if (location.variety === "Chardonnay"){
   	// chardonnayMarkers.addLayer(
@@ -92,7 +92,7 @@ var emeraldmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.p
 
 var streetsmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
+  maxZoom: 11,
   id: "mapbox.streets",
   accessToken: API_KEY
 });
@@ -125,9 +125,78 @@ var overlayMaps = {
 var myMap = L.map("map", {
 center: [40.7128, -74.0060],
   zoom: 8,  // 0 to 24
-  maxZoom: 18,
+  maxZoom: 11,
   layers: [streetsmap, burgersMarkers]  // , chardonnay, redBlend, cabernetSauvignon, bordeaux]
 });
+
+
+// var link = "http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/" +
+// "35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson";
+
+// // Function that will determine the color of a neighborhood based on the borough it belongs to
+// function chooseColor(borough) {
+//   switch (borough) {
+//   case "Brooklyn":
+//     return "yellow";
+//   case "Bronx":
+//     return "red";
+//   case "Manhattan":
+//     return "orange";
+//   case "Queens":
+//     return "green";
+//   case "Staten Island":
+//     return "purple";
+//   default:
+//     return "black";
+//   }
+// }
+
+// // Grabbing our GeoJSON data..
+// d3.json(link, function(data1) {
+//   console.log(borough)
+//   // Creating a geoJSON layer with the retrieved data
+//   L.geoJson(data1, {
+//     // Style each feature (in this case a neighborhood)
+//     style: function(feature) {
+//       return {
+//         color: "white",
+//         // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+//         fillColor: chooseColor(feature.properties.borough),
+//         fillOpacity: 0.5,
+//         weight: 1.5
+//       };
+//     },
+//     // Called on each feature
+//     onEachFeature: function(feature, layer) {
+//       // Set mouse events to change map styling
+//       layer.on({
+//         // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+//         mouseover: function(event) {
+//           layer = event.target;
+//           layer.setStyle({
+//             fillOpacity: 0.9
+//           });
+//         },
+//         // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+//         mouseout: function(event) {
+//           layer = event.target;
+//           layer.setStyle({
+//             fillOpacity: 0.5
+//           });
+//         },
+//         // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+//         click: function(event) {
+//           map.fitBounds(event.target.getBounds());
+//         }
+//       });
+//       // Giving each feature a pop-up with information pertinent to it
+//       layer.bindPopup("<h1>" + feature.properties.neighborhood + "</h1> <hr> <h2>" + feature.properties.borough + "</h2>");
+//       console.log(data1)
+
+//     }
+//   })    //.addTo(map);
+// });
+
 
 
 
